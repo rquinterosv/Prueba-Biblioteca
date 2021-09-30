cefb0d053c47b2d20afa894bb2474a6e6d1d8fb5
Rafael Quinteros <rafael.quinterosvq@gmail.com>
2021-09-30 18:02:00 -0300

    final prueba

--- biblioteca.sql
+++ biblioteca.sql
@@ -41,21 +41,15 @@ CREATE TABLE historial(
      telefono VARCHAR
  );
 
- CREATE TABLE autor_libro(
-     id INT, 
-     codi_autor INT,
-     autor_id INT,
-     FOREIGN KEY (codi_autor) REFERENCES libro(cod_autor)
- );
 
- CREATE TABLE autor(
-     id INT,
-     nombre VARCHAR,
-     apellido VARCHAR,
-     nacimiento_muerte VARCHAR,
-     Tipo_autor VARCHAR,
-     FOREIGN KEY (id) REFERENCES autor_libro(autor_id)
- );
+CREATE TABLE autor(
+  autor_id serial PRIMARY KEY,
+  nombre VARCHAR(50),
+  apellido VARCHAR(50),
+  fecha_nacimiento VARCHAR(4),
+  fecha_fallecimiento VARCHAR(4),
+  tipo VARCHAR(100)
+);
  
 
 -- Insertar registros en las tablas:
@@ -63,41 +57,32 @@ CREATE TABLE historial(
 \copy socio FROM 'socios.csv' csv;
 \copy historial FROM 'historial.csv' csv;
 
+iNSERT INTO autor(autor_id, nombre, apellido, fecha_nacimiento, fecha_fallecimiento, tipo) VALUES (3, 'JOSE', 'SALGADO', '1968', '2020', 'PRINCIPAL');
+INSERT INTO autor(autor_id, nombre, apellido, fecha_nacimiento, fecha_fallecimiento, tipo) VALUES (4, 'ANA', 'SALGADO', '1972', '', 'COAUTOR');
+INSERT INTO autor(autor_id, nombre, apellido, fecha_nacimiento, fecha_fallecimiento, tipo) VALUES (1, 'ANDRÉS', 'ULLOA', '1982', '', 'PRINCIPAL');
+INSERT INTO autor(autor_id, nombre, apellido, fecha_nacimiento, fecha_fallecimiento, tipo) VALUES (2, 'SERGIO', 'MARDONES', '1950', '2012', 'PRINCIPAL');
+INSERT INTO autor(autor_id, nombre, apellido, fecha_nacimiento, fecha_fallecimiento, tipo) VALUES (5, 'MARTIN', 'PORTA', '1976', '', 'PRINCIPAL');
+
+
 Ejercicio 3:
 a) Mostrar todos los libros que posean menos de 300 páginas. 
 
-biblioteca=# SELECT * FROM libros WHERE paginas < 300;
-
-      isbn       |         titulo         | paginas | autor_id | coautor_id 
------------------+------------------------+---------+----------+------------
- 222-2222222-222 | POESÍAS CONTEMPORANEAS |     167 |        1 |           
- 444-4444444-444 | MANUAL DE MECÁNICA     |     298 |        5 |           
-(2 rows)
+SELECT * FROM libro WHERE q_paginas < '300';
 
 -----------------------------------------------------------------------------------------
 b) Mostrar todos los autores que hayan nacido después del 01-01-1970.
 
-biblioteca=# SELECT * FROM autor WHERE to_date(fecha_nacimiento, 'yyyy') < '01-01-1970'::date;
- autor_id | nombre | apellido | fecha_nacimiento | fecha_fallecimiento |   tipo    
-----------+--------+----------+------------------+---------------------+-----------
-        3 | JOSE   | SALGADO  | 1968             | 2020                | PRINCIPAL
-        2 | SERGIO | MARDONES | 1950             | 2012                | PRINCIPAL
-(2 rows)
+SELECT * FROM autor WHERE to_date(fecha_nacimiento, 'yyyy') < '01-01-1970'::date;
+
 
 ----------------------------------------------------------------------------------------------------
 c)¿Cuál es el libro más solicitado?
 
-biblioteca=# SELECT isbn, COUNT(*) FROM prestamos GROUP BY isbn;
-      isbn       | count 
------------------+-------
- 444-4444444-444 |     2
- 111-1111111-111 |     2
- 222-2222222-222 |     2
- 333-3333333-333 |     1
-(4 rows)
+SELECT isbn, COUNT(*) FROM libro GROUP BY isbn;
+
 
 -----------------------------------------------------------------------------------------------------
 d)Si se cobrara una multa de $100 por cada día de atraso, mostrar cuánto debería pagar cada usuario que entregue el préstamo después de 7 días.
 
-SELECT socio_id, (fecha_devolucion - fecha_prestamo) * 100 AS deuda FROM historial;
+SELECT id_socio, (fecha_devolucion - fecha_prestamo) * 100 AS deuda FROM historial;
 
